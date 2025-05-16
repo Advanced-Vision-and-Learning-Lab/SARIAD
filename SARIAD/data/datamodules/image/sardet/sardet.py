bda730a0835e918a60c6d4cc84c59f10e5f33a55
Lucian Chauvin <lucianchauvin@gmail.com>
2025-05-16 12:55:40 -0500

    DATA: sardet: add in sardet downloader refs #2

---
+++ SARIAD/data/datamodules/image/sardet/sardet.py
@@ -0,0 +1,30 @@
+from anomalib.data import Folder
+from anomalib import TaskType
+from SARIAD.data import fetch_blob
+from SARIAD import DATASETS_PATH
+
+NAME = "SARDet_100K"
+KAGGLE = "greatbird/sardet-100k"
+
+class SAMPLE_PUBLIC(Folder):
+    def __init__(self, split="train", task=TaskType.SEGMENTATION):
+        self.split = split
+        self.train_batch_size = 32
+        self.eval_batch_size = 16
+        self.image_size=(0,0)
+
+        fetch_blob(NAME, kaggle=KAGGLE)
+
+        super().__init__(
+            name=NAME,
+            root=f"{DATASETS_PATH}/{NAME}/",
+            mask_dir=f"",
+            normal_dir=f"",
+            abnormal_dir=f"",
+            image_size=self.image_size,
+            train_batch_size=self.train_batch_size,
+            eval_batch_size=self.eval_batch_size,
+            task=task,
+        )
+
+        self.setup()
