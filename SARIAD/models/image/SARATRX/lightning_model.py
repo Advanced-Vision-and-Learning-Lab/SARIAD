6d2396b2aa8bec8c30a56aadc62110ad3e2ca9ce
Lucian Chauvin <lucianchauvin@gmail.com>
2025-08-04 12:57:10 -0500

    SARATRX: going to leave off here

--- SARIAD/models/image/SARATRX/lightning_model.py
+++ SARIAD/models/image/SARATRX/lightning_model.py
@@ -8,12 +8,19 @@ from SARIAD.models.image.SARATRX.SARATRX.pretraining.models.models_hivit import 
 from SARIAD.models.image.SARATRX.SARATRX.pretraining.util.pos_embed import interpolate_pos_embed
 from SARIAD.utils.blob_utils import fetch_blob
 from anomalib.post_processing import PostProcessor
+from anomalib.pre_processing import PreProcessor
+from torchvision.transforms.v2 import Compose, Resize, Grayscale
 
 logger = logging.getLogger(__name__)
 
 class SARATRX(AnomalibModule):
     def __init__(self, pre_processor=True, post_processor=True, num_classes=2):
-        super().__init__(pre_processor, post_processor)
+        transform = Compose([
+            Resize((224, 224)),
+            Grayscale(num_output_channels=1),
+        ])
+        super().__init__(PreProcessor(transform), post_processor)
+
         self.trainer_arguments = {
             "max_epochs": 100,
             "accelerator": "gpu",
@@ -23,31 +30,36 @@ class SARATRX(AnomalibModule):
             "logger": True,
         }
 
-        self.model = HiViT(num_classes=num_classes)
+        self.model = HiViTMaskedAutoencoder(hifeat=True)
+        print(self.model)
         self.outputs = []
 
         fetch_blob("mae_hivit_base_1600ep.pth", drive_file_id="1VZQz4buhlepZ5akTcEvrA3a_nxsQZ8eQ", is_archive=False);
         checkpoint = torch.load("mae_hivit_base_1600ep.pth", map_location='cpu')
         # print(checkpoint)
 
-        checkpoint_model = checkpoint
-        state_dict = self.model.state_dict()
-        print(len(state_dict.keys()))
-        for k in ['head.weight', 'head.bias']:
-            if k in checkpoint_model and checkpoint_model[k].shape != state_dict[k].shape:
-                print(f"Removing key {k} from pretrained checkpoint")
-                del checkpoint_model[k]
+        # checkpoint_model = checkpoint
+        # state_dict = self.model.state_dict()
+        # print(len(state_dict.keys()))
+        # for k in ['head.weight', 'head.bias']:
+        #     if k in checkpoint_model and checkpoint_model[k].shape != state_dict[k].shape:
+        #         print(f"Removing key {k} from pretrained checkpoint")
+        #         del checkpoint_model[k]
 
-        interpolate_pos_embed(self.model, checkpoint_model)
+        # interpolate_pos_embed(self.model, checkpoint_model)
 
-        msg = self.model.load_state_dict(checkpoint_model, strict=False)
+        msg = self.model.load_state_dict(checkpoint, strict=False)
         print(msg)
         print(self.model)
+
+    def config_pre_processor(self):
+        return PostProcessor()
 
     def configure_post_processor(self):
         return PostProcessor()
 
     def training_step(self, batch: Batch, *args, **kwargs) -> None:
+        del args, kwargs  # These variables are not used.
         output = self.model(batch.image)
         self.outputs.append(output)
 
@@ -55,6 +67,7 @@ class SARATRX(AnomalibModule):
         pass
 
     def validation_step(self, batch: Batch, *args, **kwargs) -> STEP_OUTPUT:
+        del args, kwargs  # These variables are not used.
         pass
 
     def learning_type(self):
